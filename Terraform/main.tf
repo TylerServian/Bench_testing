a8bd354182a69f40640c2f824d40d981b529bacb
Tyler Arnold <tylerarnold@Tylers-MBP.gateway>
2022-05-27 10:37:29 +1000

    Test deploy

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "helloazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "rg-hello-azure"
+  location = "australiasoutheast"
+}
 resource "azurerm_resource_group" "rg-hello-azure-tf" {
   name     = "rg-hello-azure-tf"
   location = "australiasoutheast"
+  tags = {
+    Environment = "Changed environment tag"
+  }
 }
